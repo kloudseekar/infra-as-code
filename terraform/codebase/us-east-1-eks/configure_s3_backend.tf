ede9024d675bfec837ea48575b5ca08eaad48b61
Maninder <kloudseekar@gmail.com>
2022-06-27 01:38:09 +0530

    working eks cluster with terraform , checking in the  module code as well

--- terraform/codebase/us-east-1-eks/configure_s3_backend.tf
+++ terraform/codebase/us-east-1-eks/configure_s3_backend.tf
@@ -1,7 +1,7 @@
 module "terraform_backend_bucket" {
   source = "/Users/Maninder/Personel/workspace/terraform/modules/aws/s3-bucket"
 
-  bucket = "${var.region_id}-terraform-statefile"
+  bucket = "${var.region_id}-terrraform-statefile"
   acl    = "private"
 
   versioning = {
@@ -13,7 +13,7 @@ module "terraform_backend_bucket" {
 module "terraform_lock_table" {
 source  = "/Users/Maninder/Personel/workspace/terraform/modules/aws/dynamodb-table"
 
-   name    = "${var.region_id}-terraform_locktable"
+   name    = "${var.region_id}-terrraform_locktable"
    hash_key = "LockID"
 
      attributes = [
@@ -24,3 +24,5 @@ source  = "/Users/Maninder/Personel/workspace/terraform/modules/aws/dynamodb-tab
      ]
 
 }
+
+
--- terraform/codebase/us-east-1-eks/version.tf
+++ terraform/codebase/us-east-1-eks/version.tf
@@ -1,17 +1,20 @@
 terraform {
-    required_version = ">= 0.12"
+    required_version = ">= 0.14"
     
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "> 3.0"
+      version = "> 3.20.0"
+    }
+
+        kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
     }
   }
 
   backend s3 {
-
   }
-  
 
 
 }
